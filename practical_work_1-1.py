d9461b0ed2c69ac0426a001737c670bcf5f336ee
AndreyGorban <Whiteman1989@gmail.com>
2021-12-25 10:23:46 +0200

    Added works from 1 to 6

--- practical_work_1-1.py
+++ practical_work_1-1.py
@@ -1,5 +1,13 @@
 var_int = 10
 var_float = 8.4
 var_str = "No"
+print("1.", "var_int:",var_int,"var_float:", var_float,"var_str:",var_str)
 
-print("var_int:",var_int,"var_float:", var_float,"var_str:",var_str)
+var_big = var_float * 3.5
+print("2.","var_big:", var_big)
+
+var_float -= 1
+print("3.", "var_float:", var_float)
+
+print("4.","var_int/var_float =", var_int/var_float, "var_big/var_float =", var_big/var_float)
+
